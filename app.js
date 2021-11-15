539dce9f6996f8b7ec837727cb7ebed0674ee9c9
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2021-11-14 21:15:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,223 @@
+//🌎 VARIABLES Y DOM 🌎
+
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+const resultado = document.getElementById("resultado")
+let numeroSelecionado = 0
+let primerValor = 0
+let segundoValor = 0
+let segundoNumeroSelecionado = 0
+let valorANumero
+let valorASegundoNumero
+let esDecimal = false
+let terminaPrimerValor = false
+let operacionValor
+let resultadoDeLaSuma = 0
+let resultadoDeLaResta = 0
+let resultadoDeLaDivision = 0
+let resultadoDeLaMultiplicacion = 0
+let operacion = ""
+let action = "decimal"
+
+// 🌎AGREGA EL PRIMER VALOR A OPERAR Y EL SEGUNDO VALOR A OPERAR🌎
+
+function agregarNumero(boton) {
+    if (terminaPrimerValor) {
+        segundoElemento = parseInt(boton)
+        segundoNumeroSelecionado = "".concat(segundoNumeroSelecionado, segundoElemento)
+        valorASegundoNumero = parseInt(segundoNumeroSelecionado)
+        segundoValor = valorASegundoNumero + 0
+        console.log(segundoValor)
+        resultado.textContent = `${segundoValor}`
+
+    } else {
+        primerElemento = parseInt(boton)
+        numeroSelecionado = "".concat(numeroSelecionado, primerElemento)
+        valorANumero = parseInt(numeroSelecionado)
+        primerValor = valorANumero + 0
+        console.log(primerValor)
+        resultado.textContent = `${primerValor}`
+
+    }
+}
+
+// 🌎 OPERACIONES 🌎
+
+function suma() {
+    if (terminaPrimerValor && segundoValor >= 1) {
+        resultadoDeLaSuma = primerValor + segundoValor
+        console.log(resultadoDeLaSuma)
+        primerValor = resultadoDeLaSuma
+        resultado.textContent = `${resultadoDeLaSuma}`
+        volverSegundoValorACero(resultadoDeLaSuma)
+
+    } else {
+        resultadoDeLaSuma = primerValor + 0
+        primerValor = resultadoDeLaSuma
+        resultado.textContent = `${resultadoDeLaSuma}`
+        volverSegundoValorACero(resultadoDeLaSuma)
+    }
+
+}
+
+
+function resta() {
+    if (terminaPrimerValor && segundoValor >= 1) {
+        resultadoDeLaResta = primerValor - segundoValor
+        console.log(resultadoDeLaResta)
+        primerValor = resultadoDeLaResta
+        resultado.textContent = `${resultadoDeLaResta}`
+        volverSegundoValorACero(resultadoDeLaResta)
+
+    } else {
+        resultadoDeLaResta = primerValor - 0
+        primerValor = resultadoDeLaResta
+        resultado.textContent = `${resultadoDeLaResta}`
+        volverSegundoValorACero(resultadoDeLaResta)
+
+    }
+}
+
+function division() {
+    if (terminaPrimerValor && segundoValor >= 1) {
+        resultadoDeLaDivision = primerValor / segundoValor
+        console.log(resultadoDeLaDivision)
+        primerValor = resultadoDeLaDivision
+        resultado.textContent = `${resultadoDeLaDivision}`
+        volverSegundoValorACero(resultadoDeLaDivision)
+
+    } else {
+        resultadoDeLaDivision = 0
+        primerValor = resultadoDeLaDivision
+        resultado.textContent = `${resultadoDeLaDivision}`
+        volverSegundoValorACero(resultadoDeLaDivision)
+    }
+}
+
+function multiplicacion() {
+
+    if (terminaPrimerValor && segundoValor >= 1) {
+        resultadoDeLaMultiplicacion = primerValor * segundoValor
+        primerValor = resultadoDeLaMultiplicacion
+        resultado.textContent = `${resultadoDeLaMultiplicacion}`
+        volverSegundoValorACero(resultadoDeLaMultiplicacion)
+
+    } else {
+        resultadoDeLaMultiplicacion = 1
+        primerValor = resultadoDeLaMultiplicacion
+        resultado.textContent = `${resultadoDeLaMultiplicacion}`
+        volverSegundoValorACero(resultadoDeLaMultiplicacion)
+
+    }
+}
+
+function decimal() {
+    if (terminaPrimerValor) {
+        segundoValor = segundoValor + "."
+        console.log(segundoValor)
+    } else {
+        primerValor = primerValor + "."
+        console.log(primerValor)
+    }
+}
+
+function volverACero() {
+    numeroSelecionado = 0
+    primerValor = 0
+    segundoValor = 0
+    segundoNumeroSelecionado = 0
+    valorANumero
+    valorASegundoNumero
+    esDecimal = false
+    terminaPrimerValor = false
+    operacionValor
+    resultadoDeLaSuma = 0
+    resultadoDeLaResta = 0
+    resultadoDeLaDivision = 0
+    resultadoDeLaMultiplicacion = 0
+    operacion = ""
+    resultado.textContent = `${0}`
+}
+
+function volverSegundoValorACero(resultado) {
+    numeroSelecionado = 0
+    primerValor = resultado
+    segundoValor = 0
+    segundoNumeroSelecionado = 0
+    valorANumero
+    valorASegundoNumero
+    operacionValor
+    resultadoDeLaSuma = 0
+    resultadoDeLaResta = 0
+    resultadoDeLaDivision = 0
+    resultadoDeLaMultiplicacion = 0
+    operacion = ""
+}
+// 🌎CUANDO SE SELECCIONA UNA OPERACIÓN🌎
+
+function computar(boton) {
+    terminaPrimerValor = true
+    console.log('Este valor es del boton: ', boton)
+    if (segundoValor > 0 && boton == "=") {
+        if (operacion == "+") {
+            suma()
+        } else if (operacion == "-") {
+            resta()
+        } else if (operacion == "x") {
+            multiplicacion()
+        } else if (operacion == "%") {
+            division()
+        }
+    } else if (boton == ".") {
+        // if (action === "decimal") {
+        //     if (!primerValor.includes(".")) {
+        //       display.textContent = primerValor + ".";
+        //     } else if (
+        //       previousKeyType === "operator" ||
+        //       previousKeyType === "calculate"
+        //     ) {
+        //       display.textContent = "0.";
+        //     }
+
+        //     calculator.dataset.previousKeyType = "decimal";
+        //   }
+
+        const resultadoActual = resultado.innerHTML;
+
+        if (esDecimal) {
+
+            if (!resultadoActual.includes('.')) {
+                const resultadoNuevo = resultadoActual + '.';
+                resultado.innerHTML = resultadoNuevo;
+            }
+
+        } else {
+            decimal()
+            const resultadoNuevo = resultadoActual + '.';
+            resultado.innerHTML = resultadoNuevo;
+            esDecimal = true
+        }
+
+
+    } else if (boton == "AC") {
+        volverACero()
+    }
+    else {
+        operacion = boton
+        console.log(operacion)
+    }
+
+    console.log('---------------------')
+
+}
+
+// 🌎CLICKS🌎
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => computar(boton.innerHTML))
+});
+
